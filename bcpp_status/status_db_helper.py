0d67c851b9cf2ad60e4e3c7850eeb87cdbfcc724
erikvw <ew2789@gmail.com>
2017-09-04 08:22:53 +0200

    fix field attr

--- bcpp_status/status_db_helper.py
+++ bcpp_status/status_db_helper.py
@@ -2,7 +2,6 @@ import json
 
 from bcpp_status.models import StatusHistory
 from django.core.exceptions import MultipleObjectsReturned
-from pprint import pprint
 
 
 class StatusDbHelper:
@@ -15,7 +14,7 @@ class StatusDbHelper:
                 status_date=self.report_date)
         except MultipleObjectsReturned:
             history_obj = StatusHistory.objects.filter(
-                status_datetime=self.report_date).order_by('created').last()
+                status_date=self.report_date).order_by('created').last()
         data = json.loads(history_obj.data)
         for k, v in data.items():
             setattr(self, k, v)
